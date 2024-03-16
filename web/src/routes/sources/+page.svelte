7f7f71f6ed300d3b8edf341dfe2c089f704659b2
sami <sulsami@protonmail.com>
2024-03-16 18:27:04 +0100

    web: init source page

---
+++ web/src/routes/sources/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { onMount } from 'svelte';
+	import { isLoggedIn } from '../../stores/auth.store';
+	import { goto } from '$app/navigation';
+	import { sources } from '../../stores/source.store';
+	import Source from './components/Source.svelte';
+	import { Listgroup } from 'flowbite-svelte';
+	import { getSources } from '$lib/source';
+
+	onMount(async () => {
+		if (!$isLoggedIn) goto('/');
+
+		if ($sources.length === 0) sources.set(await getSources());
+		console.log($sources);
+	});
+</script>
+
+<div class="w-full bg-neutral-800 sm:p-4">
+	<Listgroup
+		active
+		class="divide-y divide-gray-200 border-none bg-neutral-800 text-gray-300 dark:divide-gray-600"
+	>
+		{#each $sources as source}
+			<Source {source}></Source>
+		{/each}
+	</Listgroup>
+</div>
---
+++ web/src/routes/sources/components/Source.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import type { SourcePreviewDto } from '$lib/source/model';
+	import { Button, ListgroupItem } from 'flowbite-svelte';
+	import { TrashBinSolid } from 'flowbite-svelte-icons';
+
+	export let source: SourcePreviewDto;
+</script>
+
+<div class="flex justify-between p-3 align-middle">
+	<ListgroupItem
+		focusClass="focus:z-40 focus:outline-none focus:ring-2 focus:ring-primary-700 dark:focus:ring-gray-500 dark:focus:text-white"
+		hoverClass="hover:text-primary-700 hover:bg-neutral-900"
+		class="flex justify-between gap-2 border-x-neutral-500 text-base font-semibold"
+	>
+		<div class="flex flex-col gap-3">
+			<div>
+				<h1>{source.content}</h1>
+				<p>{source.type}</p>
+			</div>
+		</div>
+	</ListgroupItem>
+	<Button class="ring-neutral-800 hover:text-primary-700" size="md" color="none">
+		<TrashBinSolid size="md"></TrashBinSolid>
+	</Button>
+</div>
