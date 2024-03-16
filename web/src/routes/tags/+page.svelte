7013ecc2bcdad7bf45a4c2c59384ec3898fd08e8
sami <sulsami@protonmail.com>
2024-03-16 18:15:36 +0100

    web: init tags page

---
+++ web/src/routes/tags/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { onMount } from 'svelte';
+	import { isLoggedIn } from '../../stores/auth.store';
+	import { goto } from '$app/navigation';
+	import { tags } from '../../stores/tag.store';
+	import Tag from './components/Tag.svelte';
+	import { Listgroup } from 'flowbite-svelte';
+	import { getTags } from '$lib/tag';
+
+	onMount(async () => {
+		if (!$isLoggedIn) goto('/');
+
+		if ($tags.length === 0) tags.set(await getTags());
+		console.log($tags);
+	});
+</script>
+
+<div class="w-full bg-neutral-800 sm:p-4">
+	<Listgroup
+		active
+		class="divide-y divide-gray-200 border-none bg-neutral-800 text-gray-300 dark:divide-gray-600"
+	>
+		{#each $tags as tag}
+			<Tag {tag}></Tag>
+		{/each}
+	</Listgroup>
+</div>
--- web/src/routes/tags/components/Tag.svelte
+++ web/src/routes/tags/components/Tag.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-	import type { WordPreviewDto } from '$lib/word/model';
-	import { Button, ListgroupItem, Badge } from 'flowbite-svelte';
+	import type { TagPreviewDto } from '$lib/tag/model';
+	import { Button, ListgroupItem } from 'flowbite-svelte';
 	import { TrashBinSolid } from 'flowbite-svelte-icons';
 
-	export let word: WordPreviewDto;
+	export let tag: TagPreviewDto;
 </script>
 
 <div class="flex justify-between p-3 align-middle">
@@ -14,22 +14,8 @@
 	>
 		<div class="flex flex-col gap-3">
 			<div>
-				<h1>{word.content}</h1>
-				<p>{word.language}</p>
+				<h1>{tag.title}</h1>
 			</div>
-			<div class="flex flex-wrap gap-3">
-				{#each word.tags as tag}
-					<Badge
-						color="none"
-						class="rounded-none bg-neutral-800 p-1 text-gray-300 ring-1 ring-primary-900"
-						>{tag.title}</Badge
-					>
-				{/each}
-			</div>
-		</div>
-		<div class="flex flex-col max-sm:hidden">
-			<i><small>{word.source?.type}: {word.source?.content}</small></i>
-			<i><small>related/linked to {word.links.length} word(s) </small></i>
 		</div>
 	</ListgroupItem>
 	<Button class="ring-neutral-800 hover:text-primary-700" size="md" color="none">
