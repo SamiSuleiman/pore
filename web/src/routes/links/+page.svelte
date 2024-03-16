7690c6376d8b1e32c2c99a11a39382fdee3f56aa
sami <sulsami@protonmail.com>
2024-03-16 18:31:24 +0100

    web: added init links page

---
+++ web/src/routes/links/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { onMount } from 'svelte';
+	import { isLoggedIn } from '../../stores/auth.store';
+	import { goto } from '$app/navigation';
+	import { links } from '../../stores/link.store';
+	import Link from './components/Link.svelte';
+	import { Listgroup } from 'flowbite-svelte';
+	import { getLinks } from '$lib/link';
+
+	onMount(async () => {
+		if (!$isLoggedIn) goto('/');
+
+		if ($links.length === 0) links.set(await getLinks());
+		console.log($links);
+	});
+</script>
+
+<div class="w-full bg-neutral-800 sm:p-4">
+	<Listgroup
+		active
+		class="divide-y divide-gray-200 border-none bg-neutral-800 text-gray-300 dark:divide-gray-600"
+	>
+		{#each $links as link}
+			<Link {link}></Link>
+		{/each}
+	</Listgroup>
+</div>
---
+++ web/src/routes/links/components/Link.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import type { LinkPreviewDto } from '$lib/link/model';
+	import { Button, ListgroupItem } from 'flowbite-svelte';
+	import { TrashBinSolid } from 'flowbite-svelte-icons';
+
+	export let link: LinkPreviewDto;
+</script>
+
+<div class="flex justify-between p-3 align-middle">
+	<ListgroupItem
+		focusClass="focus:z-40 focus:outline-none focus:ring-2 focus:ring-primary-700 dark:focus:ring-gray-500 dark:focus:text-white"
+		hoverClass="hover:text-primary-700 hover:bg-neutral-900"
+		class="flex justify-between gap-2 border-x-neutral-500 text-base font-semibold"
+	>
+		<div class="flex flex-col gap-3">
+			<div>
+				<h1>{link.title}</h1>
+			</div>
+		</div>
+	</ListgroupItem>
+	<Button class="ring-neutral-800 hover:text-primary-700" size="md" color="none">
+		<TrashBinSolid size="md"></TrashBinSolid>
+	</Button>
+</div>
