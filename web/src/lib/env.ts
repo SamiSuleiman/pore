2d989fd64b91f34cd1317c8d5366b7d0a9ba16e9
mmayss <sulsami@protonmail.com>
2024-03-27 21:14:36 +0100

    fixed prod compose file

--- web/src/lib/env.ts
+++ web/src/lib/env.ts
@@ -1,17 +1,6 @@
-import { dev } from '$app/environment';
+//@ts-ignore
+import { env as e} from '$env/dynamic/public';
 
-const _prodEnv: Env = {
-	baseUrl: 'https://pore.mmayss.com/api',
-	// just for now for docker thing test
-	// baseUrl: 'http://localhost:3000/api'
-};
-
-const _devEnv: Env = {
-	baseUrl: 'http://localhost:3000/api',
-};
-
-interface Env {
-	baseUrl: string;
+export const env = {
+	baseUrl: e.PUBLIC_BASE_URL,
 }
-
-export const env = dev ? _devEnv : _prodEnv;
